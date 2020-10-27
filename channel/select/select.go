906198a78f07c4d304bd289ccc67b027bab9c896
DiangD <1304138666@qq.com>
2020-10-28 00:07:56 +0800

    update select mutex

---
+++ channel/select/select.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func worker(id int, c chan int) {
+	for n := range c {
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d received %d\n",
+			id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
+func generator() chan int {
+	out := make(chan int)
+	go func() {
+		i := 0
+		for {
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+			out <- i
+			i++
+		}
+	}()
+	return out
+}
+
+func main() {
+	c1, c2 := generator(), generator()
+	worker := createWorker(0)
+	values := make([]int, 0)
+	after := time.After(10 * time.Second)
+	tick := time.Tick(time.Second)
+	for {
+		var activeWorker chan<- int //nil channel
+		var activeValue int
+		if len(values) > 0 {
+			activeWorker = worker
+			activeValue = values[0]
+		}
+		select {
+		case n := <-c1:
+			values = append(values, n)
+		case n := <-c2:
+			values = append(values, n)
+		case activeWorker <- activeValue:
+			values = values[1:]
+		case <-time.After(800 * time.Millisecond):
+			fmt.Println("======Timeout=====")
+		case <-tick:
+			fmt.Println("Length of Queue :", len(values))
+		case <-after:
+			fmt.Println("10s CountDown dead line")
+			return
+		}
+	}
+
+}
