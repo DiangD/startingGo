457f99a58e44b62b5822a6f708f7fd1fd84fa018
DiangD <1304138666@qq.com>
2020-11-08 10:14:29 +0800

    update maze bfs comment

--- maze/maze.go
+++ maze/maze.go
@@ -5,6 +5,9 @@ import (
 	"os"
 )
 
+//BFS迷宫算法
+
+//createMaze 生成迷宫
 func createMaze(fileName string) [][]int {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -40,6 +43,7 @@ type point struct {
 	x, y int
 }
 
+//add 计算可探索范围内的点的地址
 func (p point) add(dir point) point {
 	return point{
 		p.x + dir.x,
@@ -47,6 +51,7 @@ func (p point) add(dir point) point {
 	}
 }
 
+//at 判断是否在迷宫内
 func (p point) at(maze [][]int) (int, bool) {
 	if p.x < 0 || p.x > len(maze)-1 {
 		return 0, false
@@ -57,8 +62,10 @@ func (p point) at(maze [][]int) (int, bool) {
 	return maze[p.x][p.y], true
 }
 
+//dirs 4个方向
 var dirs = [4]point{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
 
+//walk 探索
 func walk(maze [][]int, start, end point) [][]int {
 	steps := make([][]int, len(maze))
 	for i := range steps {
@@ -67,26 +74,34 @@ func walk(maze [][]int, start, end point) [][]int {
 	queue := []point{start}
 
 	for len(queue) > 0 {
+		//当前位置
 		cur := queue[0]
 		queue = queue[1:]
+		//探索到终点
 		if cur == end {
 			break
 		}
+		//探索四个方向
 		for _, dir := range dirs {
 			next := cur.add(dir)
 			val, ok := next.at(maze)
+			//遇到墙
 			if !ok || val == 1 {
 				continue
 			}
+			//已经探索过
 			val, ok = next.at(steps)
 			if !ok || val != 0 {
 				continue
 			}
+			//探索回到起点
 			if next == start {
 				continue
 			}
+			//获取到上一个探索的点在steps的值，++
 			if curStep, ok := cur.at(steps); ok {
 				steps[next.x][next.y] = curStep + 1
+				//push进队列
 				queue = append(queue, next)
 			}
 		}
