466b3d294f4741b12ac7089a4daeaec26dbd4463
DiangD <1304138666@qq.com>
2020-11-19 00:43:40 +0800

    update custom lock

---
+++ goroutine/flow/lock/lock.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Lock struct {
+	ch chan struct{}
+}
+
+func NewLock() *Lock {
+	var lock Lock
+	lock.ch = make(chan struct{}, 1)
+	lock.ch <- struct{}{}
+	return &lock
+}
+
+func (l *Lock) Lock() bool {
+	result := false
+	select {
+	case <-l.ch:
+		result = true
+	}
+	return result
+}
+
+func (l *Lock) Unlock() {
+	l.ch <- struct{}{}
+}
+
+var counter int
+
+func main() {
+	var lock = NewLock()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if !lock.Lock() {
+				// log error
+				fmt.Println("lock failed")
+				return
+			}
+			counter++
+			fmt.Println("current counter", counter)
+			lock.Unlock()
+		}()
+	}
+	wg.Wait()
+}
