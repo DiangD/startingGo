b1ffa72c59677ea822864eae4ce98342791db29a
qiuzhihan <qiuzhihan@xinyu668.com>
2020-12-03 14:41:53 +0800

    extends struct:重写

--- learngo/basic/struct/nest.go
+++ learngo/basic/struct/nest.go
@@ -2,26 +2,37 @@ package main
 
 import "fmt"
 
-type person struct {
-	sex  int
+type iAnimal interface {
+	eat()
+}
+
+type animal struct {
 	name string
 }
 
-//类似于继承
-type man struct {
-	*person
-	extra string
+func newAnimal(name string) *animal {
+	return &animal{name: name}
 }
 
-func (p person) print() {
-	fmt.Println("this is a person")
+func (a *animal) eat() {
+	fmt.Println(a.name + " is eating")
 }
+
+//内嵌结构体，可以与oop语言的继承结合理解
+type cat struct {
+	*animal
+}
+
+func newCat(name string) *cat {
+	return &cat{animal: newAnimal(name)}
+}
+
+//方法重载
+func (cat *cat) eat() {
+	fmt.Printf("children %v is eating\n", cat.name)
+}
+
 func main() {
-	m := man{}
-	m.person = &person{
-		sex:  0,
-		name: "test",
-	}
-	m.extra = "extend person"
-	fmt.Println(m.person, m.extra)
+	cat := newCat("cat")
+	cat.eat()
 }
